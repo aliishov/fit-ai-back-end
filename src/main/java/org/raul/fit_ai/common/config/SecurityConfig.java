1012a91ac47cfe753b67f987f5d80c12d74c40d2
Raul Alishov <alishov7394@gmail.com>
2026-05-13 12:13:10 +0400

    feat: split filter chain logic

--- src/main/java/org/raul/fit_ai/common/config/SecurityConfig.java
+++ src/main/java/org/raul/fit_ai/common/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package org.raul.fit_ai.common.config;
 
 import org.raul.fit_ai.auth.service.SignOutHandler;
 import org.raul.fit_ai.auth.service.jwt.JwtFilter;
+import org.raul.fit_ai.auth.service.oauth2.CustomOAuth2UserService;
+import org.raul.fit_ai.auth.service.oauth2.handler.OAuth2FailureHandler;
+import org.raul.fit_ai.auth.service.oauth2.handler.OAuth2SuccessHandler;
 import org.raul.fit_ai.auth.util.JwtProperties;
 
 import lombok.AccessLevel;
@@ -11,6 +14,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,8 +37,27 @@ public class SecurityConfig {
 
 	JwtFilter jwtFilter;
 	SignOutHandler signOutHandler;
+	CustomOAuth2UserService customOAuth2UserService;
+	OAuth2SuccessHandler oAuth2SuccessHandler;
+	OAuth2FailureHandler oAuth2FailureHandler;
 
 	@Bean
+	@Order(1)
+	public SecurityFilterChain oauth2FilterChain(HttpSecurity http) throws Exception {
+		return http
+				.securityMatcher("/oauth2/**", "/login/oauth2/**")
+				.csrf(AbstractHttpConfigurer::disable)
+				.oauth2Login(oauth2 -> oauth2
+						.userInfoEndpoint(userInfo -> userInfo
+								.userService(customOAuth2UserService)
+						)
+						.successHandler(oAuth2SuccessHandler)
+						.failureHandler(oAuth2FailureHandler)
+				)
+				.build();
+	}
+	@Bean
+	@Order(2)
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 		return http
 				.cors(Customizer.withDefaults())
