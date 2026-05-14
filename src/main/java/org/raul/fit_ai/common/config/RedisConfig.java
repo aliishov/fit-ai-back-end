c17e661f0755e27f49d55553fafedac8137c7e17
Raul Alishov <alishov7394@gmail.com>
2026-05-14 12:00:45 +0400

    feat: add cache manager config

--- src/main/java/org/raul/fit_ai/common/config/RedisConfig.java
+++ src/main/java/org/raul/fit_ai/common/config/RedisConfig.java
@@ -4,18 +4,24 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import tools.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -48,5 +54,22 @@ public class RedisConfig {
 	@Bean
 	public GenericJacksonJsonRedisSerializer genericJacksonSerializer() {
 		return new GenericJacksonJsonRedisSerializer(objectMapper);
+	}
+
+	@Bean
+	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+				.entryTtl(Duration.ofMinutes(60))
+				.serializeKeysWith(
+						RedisSerializationContext.SerializationPair
+								.fromSerializer(new StringRedisSerializer()))
+				.serializeValuesWith(
+						RedisSerializationContext.SerializationPair
+								.fromSerializer(genericJacksonSerializer()))
+				.disableCachingNullValues();
+
+		return RedisCacheManager.builder(redisConnectionFactory)
+				.cacheDefaults(config)
+				.build();
 	}
 }
