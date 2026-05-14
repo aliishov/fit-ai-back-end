d56dceeb329069979a8604d0a7c0dee679d56662
Raul Alishov <alishov7394@gmail.com>
2026-05-14 12:23:23 +0400

    fix: validation patterns

--- src/main/java/org/raul/fit_ai/auth/util/ValidationPatterns.java
+++ src/main/java/org/raul/fit_ai/auth/util/ValidationPatterns.java
@@ -4,13 +4,13 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class ValidationPatterns {
-	public static final String NAME = "^[\\\\p{L}\\\\p{M}\\\\s'-]+$";
+	public static final String NAME = "^[\\p{L}\\p{M}\\s'-]+$";
 	public static final String PHONE = "^\\+[1-9]\\d{6,14}$";
-	public static final String EMAIL = "^[a-zA-Z0-9._%+\\\\-]+@[a-zA-Z0-9.\\\\-]+\\\\.[a-zA-Z]{2,}$";
+	public static final String EMAIL = "^[a-zA-Z0-9._%+\\-]+@[a-zA-Z0-9.\\-]+\\.[a-zA-Z]{2,}$";
 	public static final String OTP = "^[0-9]{6}$";
 	public static final String RESET_TOKEN = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
 	public static final String PASSWORD_UPPERCASE = ".*[A-Z].*";
 	public static final String PASSWORD_LOWERCASE = ".*[a-z].*";
-	public static final String PASSWORD_DIGIT     = ".*\\d.*";
-	public static final String PASSWORD_SPECIAL   = ".*[@$!%*?&\\-_.+#^()|~].*";
+	public static final String PASSWORD_DIGIT = ".*\\d.*";
+	public static final String PASSWORD_SPECIAL = ".*[@$!%*?&\\-_.+#^()|~].*";
 }
