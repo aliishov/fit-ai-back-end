d0895ebae44d8630c8c89b52641f31dbede143ed
Raul Alishov <alishov7394@gmail.com>
2026-05-28 15:10:29 +0400

    feat: implement workout ai service

---
+++ src/main/java/org/raul/fit_ai/common/exception/AiResponseParseException.java
@@ -0,0 +1,11 @@
+package org.raul.fit_ai.common.exception;
+
+public class AiResponseParseException extends BaseException {
+	public AiResponseParseException(String message) {
+		super(message);
+	}
+
+	public AiResponseParseException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
