28d48465b946782806ed725be1296a8706d9259c
Raul Alishov <alishov7394@gmail.com>
2026-06-03 21:34:11 +0400

    refactor: App User Mapper

--- src/main/java/org/raul/fit_ai/auth/mapper/AppUserMapper.java
+++ src/main/java/org/raul/fit_ai/auth/mapper/AppUserMapper.java
@@ -3,19 +3,16 @@ package org.raul.fit_ai.auth.mapper;
 import org.raul.fit_ai.auth.dto.request.RegisterRequestDTO;
 import org.raul.fit_ai.auth.model.AppUser;
 
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AppUserMapper {
 
-	PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
 	public AppUser toEntity(RegisterRequestDTO request) {
 		return AppUser.builder()
