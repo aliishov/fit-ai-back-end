31ae4218a71300e788f00ad5537a85f4dd9ae59e
Raul Alishov <alishov7394@gmail.com>
2026-06-03 21:33:52 +0400

    refactor: Admin User Mapper

--- src/main/java/org/raul/fit_ai/auth/mapper/AdminUserMapper.java
+++ src/main/java/org/raul/fit_ai/auth/mapper/AdminUserMapper.java
@@ -3,9 +3,7 @@ package org.raul.fit_ai.auth.mapper;
 import org.raul.fit_ai.auth.dto.request.RegisterRequestDTO;
 import org.raul.fit_ai.auth.model.AdminUser;
 
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -14,10 +12,9 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AdminUserMapper {
 
-	PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
 	public AdminUser toEntity(UUID adminId, RegisterRequestDTO request) {
 		return AdminUser.builder()
